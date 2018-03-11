7565da878b7c22a5d97379e73c67d0181f07a7c9
zhangyiming <zhangyiming07@163.com>
2018-03-11 15:04:34 +0800

    重构项目结构

--- main.py
+++ main.py
@@ -1,92 +1,19 @@
 import datetime
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
 from flask_wtf import FlaskForm
+from sqlalchemy import func
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
-from config import DevConfig
-
+from Se7enBlog.config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 db = SQLAlchemy(app)
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255))
-    password = db.Column(db.String(255))
-    post = db.relationship('Post', backref='users', lazy='dynamic')
 
-    def __init__(self, username):
-        self.username = username
-
-    def __repr__(self):
-        return "<User `{}`>".format(self.username)
-
-
-tags = db.Table('post_tags',
-                db.Column('post_id', db.Integer(), db.ForeignKey('posts.id')),
-                db.Column('tag_id', db.Integer(), db.ForeignKey('tags.id'))
-                )
-
-
-class Post(db.Model):
-    __tablename__ = 'posts'
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
-    text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime())
-    comments = db.relationship('Comment', backref='posts', lazy='dynamic')
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
-    tags = db.relationship('Tag', secondary=tags, backref=db.backref('posts', lazy='dynamic'))
-
-    def __init__(self, title):
-        self.title = title
-
-    def __repr__(self):
-        return "<Post `{}`>".format(self.title)
-
-
-class Tag(db.Model):
-    __tablename__ = 'tags'
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255))
-
-    def __init__(self, title):
-        self.title = title
-
-    def __repr__(self):
-        return "<Tag `{}`>".format(self.title)
-
-
-class Comment(db.Model):
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(255))
-    text = db.Column(db.Text())
-    date = db.Column(db.DateTime())
-    post_id = db.Column(db.Integer(), db.ForeignKey('posts.id'))
-
-    def __repr__(self):
-        return "<Comment `{}`>".format(self.text[:15])
-
-
-def siderbar_data():
-    recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
-    top_tags = db.session.query(
-        Tag, func.count(tags.c.post_id).label('total')
-    ).join(tags).group_by(Tag).order_by('total DESC').limit(5).all()
-
-    return recent, top_tags
-
-
-class CommentForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(max=255)])
-    text = TextAreaField('Comment', validators=[DataRequired()])
 
 
 @app.route('/')
--- manage.py
+++ manage.py
@@ -1,20 +1,26 @@
+import os
 from flask_script import Manager, Server
+from flask_script.commands import ShowUrls
 from flask_migrate import Migrate, MigrateCommand
+from Se7enBlog import create_app
+from Se7enBlog.models import db, User, Post, Tag, Comment
 
-from main import app, db, User, Post, Comment, Tag
+
+env = os.environ.get('WEBAPP_ENV', 'dev')
+app = create_app('webapp.config.%sConFig' % env.capitalize())
 
 
 migrate = Migrate(app, db)
+
 manager = Manager(app)
-manager.add_command('server', Server())
-manager.add_command('db', MigrateCommand)
+manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
+manager.add_command("db", MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag)
-
-
+    return dict(app=app, db=db, User=User, Post=Post, Tag=Tag, Comment=Comment)
 
 if __name__ == '__main__':
     manager.run()
