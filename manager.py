ab245e20757a90654c2590f96b5a06094eaf3682
zhangyiming <zhangyiming07@163.com>
2018-03-11 16:31:55 +0800

    增加密码验证

--- manager.py
+++ manager.py
@@ -1,16 +1,20 @@
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager, Shell, Server
 from app import create_app, db
 from app.models import User, Role
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+manager = Manager(app)
 
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
-
+manager.add_command("server", Server())
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
 
 @app.cli.command()
 def test():
@@ -18,3 +22,6 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
